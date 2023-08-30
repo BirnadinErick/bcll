bba0d0d2effed73e86a1544fcb01cc8f8c88c4b8
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-08-30 12:18:44 +0530

    idk what I did

---
+++ src/config/bio.ts
@@ -0,0 +1,12 @@
+const name_full = "British College of Language Learning";
+const name_short = "BCLL";
+
+// contact details
+const address = "21, Palaly Road 40000 Jaffna, Sri Lanka";
+const telephone = "+94 77 706 6000";
+const email = "bclljaffna@gmail.com";
+
+// social links
+const fb_link = "https://www.facebook.com/people/BCLL/100063738556764/";
+
+export { address, name_full, name_short, telephone, email, fb_link };
